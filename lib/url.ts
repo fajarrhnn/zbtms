1625c8d7a199ec5ac4bbe387a77509d4714cd0e9
fajarrhnn <fajarrehan023@gmail.com>
2025-11-02 18:10:06 +0700

    update

--- lib/url.ts
+++ lib/url.ts
@@ -1,3 +1,1 @@
-export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-
-// process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` :
+export const baseUrl = process.env.VERCEL_URL
